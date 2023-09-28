bc094c52d7d27aabeb7553d1cf17e5ca922e2ce4
sivaram-undefy <sivaram@undefy.io>
2023-09-28 19:16:26 +0530

    bring in mint form to page

--- app/mint/page.tsx
+++ app/mint/page.tsx
@@ -1,3 +1,11 @@
+'use client'
+
+import { MintForm } from '@/components/common/form-elements/forms/MintForm'
+
 export default function MintPage() {
-  return <main className="w-full min-h-screen"></main>
+  return (
+    <main className="w-full min-h-screen px-4 py-20 flex items-center justify-center">
+      <MintForm />
+    </main>
+  )
 }
