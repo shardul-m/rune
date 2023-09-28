0b7f0686a0c25c470118049ae87c73a0560b8101
SivaramPg <sivarampg95@gmail.com>
2023-09-28 17:58:43 +0530

    update tailwind config for theming

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+import { fontFamily } from 'tailwindcss/defaultTheme'
 
 const config: Config = {
   content: [
@@ -8,10 +9,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: ['var(--font-inter)', ...fontFamily.sans],
+        mono: ['var(--font-courier-prime)', ...fontFamily.mono],
+      },
+      colors: {
+        primaryBackground: '#EBEDDE',
+        primaryText: '#342917',
       },
     },
   },
