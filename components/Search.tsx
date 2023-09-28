4887e4f2afe7f4cee4190cc33f84747043d281fa
SivaramPg <sivarampg95@gmail.com>
2023-09-28 18:09:52 +0530

    search componentt update

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,24 +1,28 @@
-"use client";
-import Link from "next/link";
-import { useState } from "react";
+'use client'
+
+import { useState } from 'react'
+import Link from 'next/link'
 
 const Search = () => {
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState('')
 
   return (
     <>
       <input
         type="text"
         className="p-2 mb-2 border-black border-2"
-        placeholder="enter tx id"
+        placeholder="Enter Tx ID"
         value={value}
         onChange={(e) => setValue(e.target.value)}
       />
-      <Link href={`/?q=${value}`} className="bg-black text-white p-2">
+      <Link
+        href={`/?q=${value}`}
+        className="bg-primaryText font-bold text-center text-white p-2"
+      >
         Search
       </Link>
     </>
-  );
-};
+  )
+}
 
-export default Search;
+export default Search
