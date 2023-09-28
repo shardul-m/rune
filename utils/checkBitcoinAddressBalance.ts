b7a4b42db58761d31b1ac0e3b50f6d1e44c42e58
sivaram-undefy <sivaram@undefy.io>
2023-09-28 23:09:04 +0530

    util method to check bitcoin address balance

---
+++ utils/checkBitcoinAddressBalance.ts
@@ -0,0 +1,20 @@
+export async function checkBitcoinAddressBalance(address: string) {
+  try {
+    const apiUrl = `https://blockstream.info/api/address/${address}/utxo`
+
+    const response = await fetch(apiUrl, { cache: 'no-cache' })
+
+    if (response.status !== 200) return null
+    const utxos = await response.json()
+
+    const balance = utxos.reduce(
+      (total: number, utxo: any) => total + utxo.value,
+      0
+    )
+
+    return balance
+  } catch (error) {
+    console.error(`An error occurred: ${error}`)
+    return null
+  }
+}
