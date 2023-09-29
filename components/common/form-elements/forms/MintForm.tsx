d4452e2f5794b0c556cea8a0a5ed167c894f03af
sivaram-undefy <sivaram@undefy.io>
2023-09-29 12:20:14 +0530

    updated functionality on mint form

--- components/common/form-elements/forms/MintForm.tsx
+++ components/common/form-elements/forms/MintForm.tsx
@@ -1,7 +1,13 @@
 'use client'
 
+import { useEffect } from 'react'
 import { FormProvider, useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
+
+import { useConnectWallet } from '@/hooks/mutation/useConnectWallet'
+import { useGetBtcToUsdExchangeRate } from '@/hooks/query/useGetBtcToUsdExchangeRate'
+import { useGetConnectedWalletInfo } from '@/hooks/query/useGetConnectedWalletInfo'
+import { useGetFeesData } from '@/hooks/query/useGetFeesData'
 
 import FormInput from '../FormInput'
 
@@ -14,14 +20,28 @@ const defaultValues: MintSchemaType = {
   tokenSymbol: '',
   maxSupply: 0,
   decimals: 0,
-  feeRate: 0,
+  feeRate: '',
 }
 
 export function MintForm({ className }: { className?: string }) {
+  const { data: feesData } = useGetFeesData()
+  const { data: exchangeRateData } = useGetBtcToUsdExchangeRate()
+  const { data: walletData } = useGetConnectedWalletInfo()
+  const { mutate } = useConnectWallet()
+
   const methods = useForm({
     defaultValues,
     resolver: zodResolver(mintSchema),
   })
+
+  const [feeRate] = methods.watch(['feeRate'])
+
+  useEffect(() => {
+    if (!feesData || feeRate) return
+    methods.setValue('feeRate', `${feesData?.halfHourFee!}-medium`)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [feesData, feeRate])
+
   return (
     <section className={cn('max-w-md w-full', className)}>
       <FormProvider {...methods}>
@@ -50,24 +70,64 @@ export function MintForm({ className }: { className?: string }) {
             placeholder="Max decimals"
           />
           <div className="">
-            <p className="mb-2">Fee Rate (6 sats/vB)</p>
+            <p className="mb-2">
+              Fee Rate ({feeRate?.split('-')?.[0]} sats/vB)
+            </p>
             <div className="justify-start items-start gap-2 flex flex-wrap">
-              <FeeButton isSelected text="Slow" />
-              <FeeButton isSelected={false} text="Medium" />
-              <FeeButton isSelected={false} text="Fast" />
+              <FeeButton
+                isSelected={feeRate === `${feesData?.hourFee}-slow`}
+                text="Slow"
+                onClick={() =>
+                  methods.setValue('feeRate', `${feesData?.hourFee}-slow`)
+                }
+              />
+              <FeeButton
+                isSelected={feeRate === `${feesData?.halfHourFee}-medium`}
+                text="Medium"
+                onClick={() =>
+                  methods.setValue('feeRate', `${feesData?.halfHourFee}-medium`)
+                }
+              />
+              <FeeButton
+                isSelected={feeRate === `${feesData?.fastestFee}-fast`}
+                text="Fast"
+                onClick={() =>
+                  methods.setValue('feeRate', `${feesData?.fastestFee}-fast`)
+                }
+              />
             </div>
           </div>
-          <p className="">Total Cost: 0.00001878 BTC ($0.50)</p>
-          <button
-            type="submit"
-            disabled={false}
-            className={cn(
-              'mt-2 border-2 border-black dark:border-white dark:text-white rounded py-2 px-5 font-bold text-lg duration-150 hover:shadow-md flex items-center justify-center gap-4',
-              'uppercase'
+          <p className="">
+            Total Cost: 0.00001878 BTC ($
+            {((exchangeRateData?.bitcoin?.usd ?? 0) * 0.00001878).toPrecision(
+              2
             )}
-          >
-            Please enter a valid receive address
-          </button>
+            )
+          </p>
+          {walletData?.addresses?.length ? (
+            <button
+              type="submit"
+              disabled={false}
+              className={cn(
+                'mt-2 border-2 border-black dark:border-white dark:text-white rounded py-2 px-5 font-bold text-lg duration-150 hover:shadow-md flex items-center justify-center gap-4',
+                'uppercase'
+              )}
+            >
+              Deploy
+            </button>
+          ) : (
+            <button
+              type="button"
+              disabled={false}
+              className={cn(
+                'mt-2 border-2 border-black dark:border-white dark:text-white rounded py-2 px-5 font-bold text-lg duration-150 hover:shadow-md flex items-center justify-center gap-4',
+                'uppercase'
+              )}
+              onClick={() => mutate()}
+            >
+              Login
+            </button>
+          )}
         </form>
       </FormProvider>
     </section>
@@ -85,9 +145,10 @@ function FeeButton({
 }) {
   return (
     <button
+      type="button"
       className={cn(
         'px-3 py-2 bg-stone-800 rounded-lg border hover:shadow border-stone-800',
-        'justify-center items-center gap-1.5 flex duration-100',
+        'justify-center items-center gap-1.5 flex duration-100 grow',
         isSelected ? 'bg-opacity-100 shadow-lg' : 'bg-opacity-10 shadow-none'
       )}
       onClick={onClick}
